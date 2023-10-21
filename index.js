4bc73a7c38a8fcc4f60a3acf0dad78c9b5c8df4a
Maniprithvi <007maniprithvi@gmail.com>
2023-10-21 21:30:16 +0530

    frst commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// //  firebase credetials
+ 
+
+const form = document.querySelector('form')
+//   const signUp = document.getElementById('signUp');
+//   console.log(signUp);
+
+
+form.addEventListener('submit',(e)=>{
+console.log("clicked");
+})
