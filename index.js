edaabb983e868663b9b5fe4f9c59eac257fb9953
Maniprithvi <111896635+Maniprithvi@users.noreply.github.com>
2023-10-21 21:38:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 
-// //  firebase credetials
- 
-
-const form = document.querySelector('form')
-//   const signUp = document.getElementById('signUp');
-//   console.log(signUp);
-
-
-form.addEventListener('submit',(e)=>{
-console.log("clicked");
-})
